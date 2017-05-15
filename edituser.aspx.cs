3bc16ef361755ac303ee6c6ba615d31019d77411
strickerlee <lixiangabc@hotmail.com>
2017-05-15 19:56:03 +0800

    add users, useredit page

---
+++ edituser.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class edituser : System.Web.UI.Page
+{
+    int userID;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        userID = int.Parse(Request.QueryString["userID"]);
+        if (userID != (int)Session["userID"] && Session["currentUser"].ToString() != "admin")
+        {
+            Response.Write(@"<script>alert('你无权修改！');</script>");
+            Server.Transfer("forum.aspx");
+            return;
+        }
+        if (!IsPostBack)
+        {
+
+            using (SQLiteConnection dbConnection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+            {
+                using (SQLiteCommand comm = dbConnection.CreateCommand())
+                {
+                    dbConnection.Open();
+                    comm.CommandText = "SELECT * FROM users WHERE id = " + userID;
+                    SQLiteDataReader dataReader = comm.ExecuteReader();
+                    dataReader.Read();
+                    username.Text = dataReader.GetString(1);
+                    email.Text = dataReader.GetString(3);
+                    school.Text = dataReader.GetString(4);
+                    major.Text = dataReader.GetString(5);
+                    grade.Text = "" + dataReader.GetInt32(6);
+                    stuID.Text = dataReader.GetString(7);
+                    dbConnection.Close();
+                }
+            }
+        }
+    }
+
+    protected void Comfirm_Click(object sender, EventArgs e)
+    {
+        // Check valid
+        if (email.Text == "" || school.Text == "" ||
+            major.Text == "" || grade.Text == "" || stuID.Text == "" ||
+            password.Text == "")
+        {
+            Response.Write(@"<script>alert('全部都不能为空哦！');</script>");
+            return;
+        }
+        int Grade;
+        if (!int.TryParse(stuID.Text, out Grade) || !int.TryParse(grade.Text, out Grade))
+        {
+            Response.Write(@"<script>alert('年级和学号必须为数字！');</script>");
+            return;
+        }
+
+        // Store in db
+        using (SQLiteConnection userDBConnection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+        {
+            using (SQLiteCommand comm = userDBConnection.CreateCommand())
+            {
+                GC.Collect();
+                userDBConnection.Open();
+
+                // md5 password
+                string md5passwd;
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] hashData = md5.ComputeHash(Encoding.Default.GetBytes(password.Text));
+                    StringBuilder stringbuilder = new StringBuilder();
+                    for (int i = 0; i < hashData.Length; i++)
+                    {
+                        stringbuilder.Append(hashData[i].ToString());
+                    }
+                    md5passwd = stringbuilder.ToString();
+                }
+
+                comm.CommandText = "UPDATE users SET password=@password, email=@email, school=@school, major=@major, " +
+                    "grade=@grade, stuID=@stuID WHERE id=" + userID;
+                comm.Parameters.AddWithValue("@password", md5passwd);
+                comm.Parameters.AddWithValue("@email", email.Text);
+                comm.Parameters.AddWithValue("@school", school.Text);
+                comm.Parameters.AddWithValue("@major", major.Text);
+                comm.Parameters.AddWithValue("@grade", Grade);
+                comm.Parameters.AddWithValue("@stuID", stuID.Text);
+                comm.ExecuteNonQuery();
+                Response.Write(@"<script>alert('修改成功！');</script>");
+                Server.Transfer("Default.aspx");
+            }
+        }
+    }
+}
---
+++ users.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class users : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["currentUser"].ToString() != "admin")
+        {
+            Response.Write(@"<script>alert('你无权访问！');</script>");
+            Server.Transfer("forum.aspx");
+            return;
+        }
+        using (SQLiteConnection dbConnection = new SQLiteConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+        {
+            using (SQLiteCommand comm = dbConnection.CreateCommand())
+            {
+                dbConnection.Open();
+                comm.CommandText = "SELECT * FROM users";
+                SQLiteDataReader dataReader = comm.ExecuteReader();
+                string eachUser = @"<a href=" + "\"userinfo.aspx?userID=@userID\"" + @">@username</a> @email @school @major @grade @stuID";
+                while (dataReader.Read())
+                {
+                    string temp = eachUser;
+                    temp = temp.Replace("@userID", dataReader.GetInt32(0).ToString());
+                    temp = temp.Replace("@username", dataReader.GetString(1));
+                    temp = temp.Replace("@email", dataReader.GetString(3));
+                    temp = temp.Replace("@school", dataReader.GetString(4));
+                    temp = temp.Replace("@major" ,dataReader.GetString(5));
+                    temp = temp.Replace("@grade", dataReader.GetInt32(6).ToString());
+                    temp = temp.Replace("@stuID", dataReader.GetString(7));
+                    PlaceHolder.Controls.Add(new LiteralControl(temp));
+                }
+                dbConnection.Close();
+            }
+        }
+    }
+}
